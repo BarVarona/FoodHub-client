489da11e9a2f605f87909ba915c659a103dd043d
Bar Varona <bar13varona@gmail.com>
2022-06-10 13:45:39 +0300

    Update App

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import GeneralPage from './pages/GeneralPage';
+import { ROUTES } from './consts/routes.const';
+import { BrowserRouter as Router, Route, Navigate, Routes } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
+
   return (
-    <div className="App">
+    <div className="App fill-window" style={{height: '100%'}}>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-      <GeneralPage/>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Navigate replace to={ROUTES.DASHBOARD} />} />,
+            <Route exact key={ROUTES.LOGIN} path={ROUTES.LOGIN} element={<LoginPage/>}/>
+            <Route exact key={ROUTES.SIGNUP} path={ROUTES.SIGNUP} element={<SignupPage/>}/>
+            <Route exact key={ROUTES.DASHBOARD} path={ROUTES.DASHBOARD} element={<GeneralPage/>}/>
+          </Routes>
+      </Router>
     </div>
   );
 }
